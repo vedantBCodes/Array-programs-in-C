ad217014a82d5c496aa5abdcc53ae80d38e44e4b
vedantBCodes <bichkundevedant97@gmail.com>
2023-03-22 14:16:39 -0700

    new programs added

---
+++ 2D Array/transposeOfMatrix.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void transposeOfMatrix(int arr[10][10],int rows,int cols)
+{
+    int i,j;
+    printf("Entered matrix is:\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Transpose of entered matrix:\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            printf("%d ",arr[j][i]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int i,j,rows,cols,arr[10][10];
+    printf("Enter the number of rows:");
+    scanf("%d",&rows);
+    printf("Enter the number of cols:");
+    scanf("%d",&cols);
+    printf("Enter %d array elements:",rows*cols);
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+transposeOfMatrix(arr,rows,cols);
+}
+
